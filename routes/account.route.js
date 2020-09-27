11b511149389258cc272d8af281ff9d9ca4e73bd
dlmtruong1609 <hinatosss111@gmail.com>
2020-09-27 21:34:57 +0700

    complete api sigin - singup

--- routes/account.route.js
+++ routes/account.route.js
@@ -7,10 +7,11 @@ const accountServiceProxy = httpProxy('http://api_account_chat:3333');
 //  account api
 router.post("/api/v0/accounts/signin", accountServiceProxy);
 router.post("/api/v0/accounts/signup", accountServiceProxy);
-router.post("/api/v0/accounts/active", authMiddleWare.isAuth, accountServiceProxy);
+router.post("/api/v0/accounts/active", accountServiceProxy);
 router.get("/api/v0/accounts/active/send", accountServiceProxy);
 router.get("/api/v0/accounts/passwords/forgot", accountServiceProxy);
 router.post("/api/v0/accounts/passwords/change", authMiddleWare.isAuth, accountServiceProxy);
+router.post("/api/v0/accounts/code/verify", accountServiceProxy);
 
 //  user api
 router.put("/api/v0/users/update", authMiddleWare.isAuth, accountServiceProxy);

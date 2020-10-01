bc142b054c75edc25004dd767d5a2dced89af71e
quannguyen1999 <nguyendanganhquan99@gmail.com>
2020-10-01 20:40:44 +0700

    add eslint

--- helpers/jwt.helper.js
+++ helpers/jwt.helper.js
@@ -1,11 +1,11 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 /**
  * private function generateToken
  * @param user
  * @param secretSignature
  * @param tokenLife
  */
-let generateToken = (user, secretSignature, tokenLife) => {
+const generateToken = (user, secretSignature, tokenLife) => {
   return new Promise((resolve, reject) => {
     // Định nghĩa những thông tin của user mà bạn muốn lưu vào token ở đây
     const userData = {
@@ -15,36 +15,36 @@ let generateToken = (user, secretSignature, tokenLife) => {
     }
     // Thực hiện ký và tạo token
     jwt.sign(
-      {data: userData},
+      { data: userData },
       secretSignature,
       {
-        algorithm: "HS256",
-        expiresIn: tokenLife, // hết hạn
+        algorithm: 'HS256',
+        expiresIn: tokenLife // hết hạn
       },
       (error, token) => {
         if (error) {
-          return reject(error);
+          return reject(error)
         }
-        resolve(token);
-    });
-  });
+        resolve(token)
+      })
+  })
 }
 /**
  * This module used for verify jwt token
  * @param {*} token
  * @param {*} secretKey
  */
-let verifyToken = (token, secretKey) => {
+const verifyToken = (token, secretKey) => {
   return new Promise((resolve, reject) => {
     jwt.verify(token, secretKey, (error, decoded) => {
       if (error) {
-        return reject(error);
+        return reject(error)
       }
-      resolve(decoded);
-    });
-  });
+      resolve(decoded)
+    })
+  })
 }
 module.exports = {
   generateToken: generateToken,
-  verifyToken: verifyToken,
-};
+  verifyToken: verifyToken
+}
--- routes/account.route.js
+++ routes/account.route.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const httpProxy = require('express-http-proxy');
-const authMiddleWare = require("../middleware/auth.middleware");
-const accountServiceProxy = httpProxy('http://api_account_chat:3333');
+const express = require('express')
+const router = express.Router()
+const httpProxy = require('express-http-proxy')
+const authMiddleWare = require('../middleware/auth.middleware')
+const accountServiceProxy = httpProxy('http://api_account_chat:3333')
 
 //  account api
-router.post("/api/v0/accounts/signin", accountServiceProxy);
-router.post("/api/v0/accounts/signup", accountServiceProxy);
-router.post("/api/v0/accounts/active", accountServiceProxy);
-router.get("/api/v0/accounts/active/send", accountServiceProxy);
-router.get("/api/v0/accounts/passwords/forgot", accountServiceProxy);
-router.post("/api/v0/accounts/passwords/change", authMiddleWare.isAuth, accountServiceProxy);
-router.post("/api/v0/accounts/code/verify", accountServiceProxy);
+router.post('/api/v0/accounts/signin', accountServiceProxy)
+router.post('/api/v0/accounts/signup', accountServiceProxy)
+router.post('/api/v0/accounts/active', accountServiceProxy)
+router.get('/api/v0/accounts/active/send', accountServiceProxy)
+router.get('/api/v0/accounts/passwords/forgot', accountServiceProxy)
+router.post('/api/v0/accounts/passssswords/change', authMiddleWare.isAuth, accountServiceProxy)
+router.post('/api/v0/accounts/code/verify', accountServiceProxy)
 
 //  user api
-router.put("/api/v0/users/update", authMiddleWare.isAuth, accountServiceProxy);
-router.get("/api/v0/users/profile", accountServiceProxy);
-router.get("/api/v0/users/search", accountServiceProxy);
-router.get("/api/v0/users/list", accountServiceProxy);
+router.put('/api/v0/users/update', authMiddleWare.isAuth, accountServiceProxy)
+router.get('/api/v0/users/profile', accountServiceProxy)
+router.get('/api/v0/users/search', accountServiceProxy)
+router.get('/api/v0/users/list', accountServiceProxy)
 
-router.post("/api/v0/users/add",authMiddleWare.isAuth,accountServiceProxy);
-router.get("/api/v0/users/getAllUser",accountServiceProxy);
-router.get("/api/v0/users/findUserByPhone", accountServiceProxy);
-router.put("/api/v0/users/updateByPhone",accountServiceProxy);
-module.exports = router;
+router.post('/api/v0/users/add', authMiddleWare.isAuth, accountServiceProxy)
+router.get('/api/v0/users/getAllUser', accountServiceProxy)
+router.get('/api/v0/users/findUserByPhone', accountServiceProxy)
+router.put('/api/v0/users/updateByPhone', accountServiceProxy)
+module.exports = router
